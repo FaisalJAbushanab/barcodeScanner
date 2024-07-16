d1949731bc3477aeba6a90438b5a26ba5c68ec2a
Faisal Abushanab <faisal-2052@hotmail.com>
2024-07-16 22:51:00 +0300

    init

---
+++ scanner.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+    Quagga.init({
+        inputStream: {
+            name: "Live",
+            type: "LiveStream",
+            target: document.querySelector('#interactive')    // Or '#yourElement' (optional)
+        },
+        decoder: {
+            readers: ["code_128_reader", "ean_reader", "ean_8_reader", "code_39_reader", "code_39_vin_reader", "codabar_reader", "upc_reader", "upc_e_reader", "i2of5_reader"]
+        }
+    }, function(err) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.log("Initialization finished. Ready to start");
+        Quagga.start();
+    });
+
+    Quagga.onDetected(function(data) {
+        console.log(data);
+        document.getElementById('result').innerText = `Detected: ${data.codeResult.code}`;
+    });
+});
